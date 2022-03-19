4e674fb391b3cc6892a68f99459f99e0432fcfa5
HovsepStepanian <hovsepstepanian@gmail.com>
2022-03-19 18:48:31 +0300

    first commit

---
+++ build/precache-manifest.67964dd7e3e086898616e81ca3c91958.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc805839ac5f7c992f016b72b641f806",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02bfba8086f801afc947",
+    "url": "/static/css/main.262cd76d.chunk.css"
+  },
+  {
+    "revision": "7ee9b6d42485e2f73a38",
+    "url": "/static/js/2.87df44be.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.87df44be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02bfba8086f801afc947",
+    "url": "/static/js/main.e96cf6ac.chunk.js"
+  },
+  {
+    "revision": "1d48a54a47b409b12466",
+    "url": "/static/js/runtime-main.1e54eb7f.js"
+  }
+]);
